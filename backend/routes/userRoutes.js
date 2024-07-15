c56c24162354987e257d6603baca393314d80d38
Devanshjoshi2804 <141642062+Devanshjoshi2804@users.noreply.github.com>
2024-07-15 19:29:43 +0530

    Delete backend directory

--- backend/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { registerUser, loginUser } = require('../controllers/userController'); // Ensure the path is correct
-
-router.post('/register', registerUser);
-router.post('/login', loginUser);
-
-module.exports = router;
--- backend/tutorU/myapp/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import PopupForm
-
-class PopupFormForm(forms.ModelForm):
-    class Meta:
-        model = PopupForm
-        fields = ['name', 'email', 'message']
